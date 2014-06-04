8b289934a565796d33fa75e0f83a7151be212f46
Thomas Albrecht <radi_@web.de>
2014-06-04 19:46:45 +1000

    ascii char texture and path fixed

--- ac3d.py
+++ ac3d.py
@@ -100,7 +100,7 @@ class Object(object):
 
 class Label(Object):
     def __init__(self):
-        super(Label, self).__init__('label', texture='ascii.png')
+        super(Label, self).__init__('label', texture='tex/ascii.png')
         self.char_w = 1.
         self.char_h = 1.
 
