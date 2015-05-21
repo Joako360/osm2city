318a48d878d2914a8f752fcc90b316b3b91f3bbe
Thomas Albrecht <radi_@web.de>
2015-05-21 23:33:23 +1000

    sharpen works nicely!

--- genmips.sh
+++ genmips.sh
@@ -31,20 +31,26 @@ genmips ()
     # nvDXT -nmips 10 -flip -file atlas_facades.png
     bd=$PWD
     case=$1
+    bf=$2
+    be=$3
+    cf=$4
+    ce=$5
     mkdir -p tmp/
     cd tmp
     cp $bd/tex/$case.png .
     #nvDXT -nmips 10 -flip -file $case.png
     #exit
     #nvDXT -nomipmap -file ${case}_01.png 
-    nvDXT -nmips 10 -flip -file $case.png
+    nvDXT -nmips 10 -flip -sharpenMethod SharpenMedium -file $case.png
     detach $case
     for i in `seq -w 0 8`; do
         is=`printf "%02i" $i`
         #(( bc = $i * 7 ))
-        bc=`awk -v num=$i 'BEGIN{print 4*num^(1.3)}'`
+        br=`awk -v num=$i 'BEGIN{print '$bf'*num^('$be')}'`
+        co=`awk -v num=$i 'BEGIN{print '$cf'*num^('$ce')}'`
+        echo "br=$br co=$co"
         mip=${case}_$is
-        convert $mip.dds -brightness-contrast ${bc}x${bc} $mip.png
+        convert $mip.dds -brightness-contrast ${br}x${co} $mip.png
         rm $mip.dds
         nvDXT -nomipmap -file $mip.png
         ls -l $mip.dds
@@ -54,5 +60,26 @@ genmips ()
     cd ..
 }
 
-genmips roads_LM
-genmips atlas_facades_LM
+gendds ()
+{
+    for i in `seq -w 0 08`; do
+        mip=${case}_$i
+        nvDXT -nomipmap -file $mip.png
+    done
+}
+
+genmips atlas_facades_LM 2. 1.7 7. 1.
+
+#genmips roads_LM 2 1.5 1 1.5
+genmips roads_LM 1 1. 1 1.
+exit
+bd=$PWD
+cd tmp
+case=roads_LM
+gendds $case
+stitch $case
+mv $case.dds $bd/tex/
+exit
+
+
+#genmips atlas_facades_LM 1.2 1.7 7. 1.
